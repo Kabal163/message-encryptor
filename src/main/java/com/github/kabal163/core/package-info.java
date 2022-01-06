93f27d3e92a6c02bf801baaa1385043ac11cdc6d
Dmitrii <cs159753@yandex.ru>
2022-01-06 19:56:53 +0300

    refactoring: added java docs

---
+++ src/main/java/com/github/kabal163/core/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the business logic of the application.
+ * The main component is the {@link com.github.kabal163.core.Processor}
+ * which is responsible for request's payload encryption.
+ * Use {@link com.github.kabal163.core.ProcessorManager#start(com.github.kabal163.core.channel.ChannelsHolder, com.github.kabal163.config.Config)}
+ * to run the processors.
+ * Use {@link com.github.kabal163.core.ProcessorManager#stop()} to stop them.
+ */
+package com.github.kabal163.core;
