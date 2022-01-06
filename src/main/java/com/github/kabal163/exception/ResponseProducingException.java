f0f96fffea0943e614de4cd96e0c4c9d66821b46
Dmitrii <cs159753@yandex.ru>
2022-01-06 20:05:24 +0300

    refactoring: unused exception

--- src/main/java/com/github/kabal163/exception/ResponseProducingException.java
+++
@@ -1,8 +0,0 @@
-package com.github.kabal163.exception;
-
-public class ResponseProducingException extends RuntimeException {
-
-    public ResponseProducingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
